d8bddab2a9715ebfbe39c0e84cf215229e223192
ericsuwardi90 <ericsuwardi90@gmail.com>
2017-08-27 13:09:08 +0700

    run rake task, change hello action in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "hello, world! You can access static pages via /static_pages /home or /help"
   end
 end
